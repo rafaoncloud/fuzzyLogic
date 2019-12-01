465f903f721996869f01e18f3cdd105d870f7b3b
Rafael Henriques <rfchenriques@gmail.com>
2019-12-01 19:35:11 +0000

    update

---
+++ part-B/computeDescreteFunction.m
@@ -0,0 +1,22 @@
+% Find the discrete transfer function
+function computeDescreteFunction()
+
+    numerator = [1 0.5]
+    denominator = [1 4.6 4.95 0.4]
+    Ts = 0; % Sampling time
+    
+    %% Compute best value for sampling time
+    
+    % 1: Compute roots of the denominator
+    denroots = roots(denominator)
+    % 2: Compute the time constants of the system
+    timeconstants = -1./denroots % inverse of elements inside vector
+    % 3: time sample time should be less than
+    % the lowest time constant, say 1/10
+    lowestimeconstant = min(timeconstants)
+    Ts = lowestimeconstant / 10
+
+    %% Apply discretization method 'zoh'
+    [numd,dend] = c2dm(numerator, ...
+        denominator, Ts,'zoh')
+end
